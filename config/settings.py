da98b3dd49e767f2d48ce4e73eb1c64a647193de
1ndoryu <andoryyu@gmail.com>
2025-03-31 08:43:48 -0400

    *

--- config/settings.py
+++ config/settings.py
@@ -112,11 +112,11 @@ RUTACLON = os.path.join(RUTA_BASE_PROYECTO, 'clonProyecto')
 RUTAHISTORIAL = os.path.join(RUTA_BASE_PROYECTO, 'historial_refactor.log')
 
 # --- Configuracion de Git ---
-RAMATRABAJO = "refactor-test-18"  # Nombre de la rama donde se aplicarán los cambios
+RAMATRABAJO = "refactor-test-19"  # Nombre de la rama donde se aplicarán los cambios
 
 # --- Configuracion de Gemini ---
-# MODELOGEMINI = os.getenv("GEMINI_MODEL", "gemini-2.5-pro-exp-03-25") # Mantener configurable el modelo
-MODELOGEMINI = "gemini-2.0-flash"
+MODELOGEMINI = os.getenv("GEMINI_MODEL", "gemini-2.5-pro-exp-03-25") # Mantener configurable el modelo
+# MODELOGEMINI = "gemini-2.0-flash"
 N_HISTORIAL_CONTEXTO = 30  # Cuántas entradas recientes del historial enviar a Gemini
 
 # --- Configuracion de Analisis (Opcional - No se usa por ahora) ---
