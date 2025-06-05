ac6f9cc67e3f14638981be834d4ff900805189a2
1ndoryu <andoryyu@gmail.com>
2025-06-05 01:17:43 -0400

    *

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ OPENROUTER_TITLE = os.getenv("OPENROUTER_TITLE", "<YOUR_APP_NAME>")     # Reempl
 OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL", "google/gemini-2.5-pro-exp-03-25:free")
 
 # --- Configuracion Esencial ---
-REPOSITORIOURL = os.getenv("REPOSITORIOURL", "git@github.com:1ndoryu/glory.git")
+REPOSITORIOURL = os.getenv("REPOSITORIOURL", "git@github.com:1ndoryu/refactor.git")
 
 # --- Configuracion de Rutas ---
 RUTA_BASE_PROYECTO = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,7 +153,7 @@ RUTACLON = os.getenv("RUTACLON", os.path.join(RUTA_BASE_PROYECTO, 'clonProyecto'
 RUTAHISTORIAL = os.getenv("RUTAHISTORIAL", os.path.join(RUTA_BASE_PROYECTO, 'historial_refactor_adaptativo.log')) # Nombre de log adaptado
 
 # --- Configuracion de Git ---
-RAMATRABAJO = os.getenv("RAMATRABAJO", "main")
+RAMATRABAJO = os.getenv("RAMATRABAJO", "Orion")
 
 # --- Configuracion de Modelos IA ---
 MODELO_GOOGLE_GEMINI = os.getenv("GEMINI_MODEL", "gemini-2.5-flash-preview-05-20")
